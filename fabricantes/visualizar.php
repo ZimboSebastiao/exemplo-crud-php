c1f22ec3da9579afba1dfe40f2e514b13ff26718
Zimbo Sebastião <gloriasebastiao7@gmail.com>
2023-08-24 08:45:31 -0300

    utilizando o comando select para visualização de dos dados dos fabricantes

---
+++ fabricantes/visualizar.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fabricantes - Visualização</title>
+</head>
+<body>
+    <p><a href="../index.php">Home</a></p>
+    <hr>
+    <h1>Fabricantes | Select</h1>
+    <hr>
+    <h2>Lendo e carregando todos os fabricantes.</h2>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CRUD PHP e SQL</title>
+</head>
+<body>
+    <h1>Sistema Vendas - CRUD PHP e SQL</h1>
+    <hr>
+
+    <p>Operações de <b>inserção</b></p>
+    <p>Leitura <b>atualização</b></p>
+    <p>Exclução de <b>dados</b></p>
+    <hr>
+
+    <h2>Escolha o que deseja gerenciar:</h2>
+
+    <ul>
+        <li><a href="fabricantes/visualizar.php">Fabricantes</a></li>
+        <li><a href="">Produtos</a></li>
+    </ul>
+</body>
+</html>
