61d7e48b8e1f8514358b53359315862a3f1ec35a
Zimbo Sebastião <gloriasebastiao7@gmail.com>
2023-08-25 08:54:58 -0300

    Update inserir.php

--- fabricantes/inserir.php
+++ fabricantes/inserir.php
@@ -1,3 +1,13 @@
+<?php 
+
+
+if( isset($_POST['inserir']) ){
+    // Capturando o valor digitado do nome e sanatizando
+    $nome = filter_input(INPUT_POST, "nome", FILTER_SANITIZE_SPECIAL_CHARS);
+    
+}
+
+?>
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -8,12 +18,14 @@
 <body>
     <h1>Fabricantes | Insert</h1>
     <hr>
+
     <form action="" method="post">
         <p>
             <label for="nome">Nome:</label>
-            <input type="text" name="nome" id="nome">
+            <input type="text" name="nome" id="nome" required>
         </p>
         <button type="submit" name="inserir">Inserir fabricantes</button>
     </form>
+
 </body>
 </html>
