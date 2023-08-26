19268ef3c4f69784b39d69d5426fd34a015020a6
Zimbo Sebastião <gloriasebastiao7@gmail.com>
2023-08-25 21:37:54 -0300

    estilizando pagina index

--- index.php
+++ index.php
@@ -4,21 +4,72 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CRUD PHP e SQL</title>
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-    <h1>Sistema Vendas - CRUD PHP e SQL</h1>
-    <hr>
+    <!-- ======== HEADER ========= -->
+    
+    <header>
+        <nav class="navbar bg-body-tertiary">
+            <div class="container">
+                <a class="navbar-brand" href="#">
+                <img src="img/palanc.png" 
+                width="90" height="84">
 
-    <p>Operações de <b>inserção</b></p>
-    <p>Leitura <b>atualização</b></p>
-    <p>Exclução de <b>dados</b></p>
-    <hr>
+                </a>
+            </div>
+        </nav>
+    </header> <!-- FIM HEADER  -->
 
-    <h2>Escolha o que deseja gerenciar:</h2>
+    <main>
 
-    <ul>
-        <li><a href="fabricantes/visualizar.php">Fabricantes</a></li>
-        <li><a href="">Produtos</a></li>
-    </ul>
+    <div class="container-xxl contneir">
+        <br>
+        <div class="container estilo">
+            <p class="hero">Gereciamento de Produtos/Fabricantes</p>
+            <h2 class="format">Sistema Vendas - CRUD PHP e SQL</h2>
+        </div>
+        <br>
+    </div>
+    <br>
+
+    <div class="card">
+        <div class="card-header">
+            <h4>Escolha o que deseja gerenciar</h4>
+        </div>
+        <div class="card-body">
+            <h5 class="card-title">Special title treatment</h5>
+            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+
+        <div class="accordion" id="accordionPanelsStayOpenExample">
+            <div class="accordion-item">
+                <h2 class="accordion-header">
+                <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
+                    Opções
+                </button>
+                </h2>
+                <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show">
+                <div class="accordion-body">
+                <div class="list-group">
+                    <a href="fabricantes/visualizar.php" class="list-group-item list-group-item-action" aria-current="true">Fabricantes</a>
+                    <a href="#" class="list-group-item list-group-item-action">Produtos</a>
+                </div>
+                </div>
+                </div>
+            </div>
+        </div>
+        </div>
+    </div>
+    
+    </main>
+
+
+
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+  </body>
 </body>
 </html>
