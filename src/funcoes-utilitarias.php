f38e0b309cf62c4266d01c38a05ac52b6d0eda1a
Zimbo Sebastião <gloriasebastiao7@gmail.com>
2023-08-29 09:35:57 -0300

    Update funcoes-utilitarias.php

--- src/funcoes-utilitarias.php
+++ src/funcoes-utilitarias.php
@@ -1,6 +1,6 @@
 <?php
 
-$formataPreco = function(float $valor){
+$formataPreco = function(float $valor):string{
 
     $precoFormatado = "R$ ".number_format($valor, 2, ",", ".");
     return $precoFormatado;
